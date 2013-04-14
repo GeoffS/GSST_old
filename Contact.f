936b78f54a4ce38323fe2670e59591ab20cdd4a7
U-MTP-WS1\Geoff <Geoff@MTP-WS1.(none)>
2013-04-14 10:52:07 -0500

    Basically a copy of DNCM with a couple of minor/non-functional changes. The produced web-site is exactly dncompositemasts.com_new only the dir is now wemc.com_new.

---
+++ Contact.f
@@ -0,0 +1,13 @@
+<fragment>
+<div class="contact">
+<p class="normal">You can contact Bob Rast at <span class="inlineCompanyName">RKR Composites</span>:</p>
+<p class="normal contactType">E-Mail: <span><a href="mailto:bob@dncompositemasts.com">bob@dncompositemasts.com</a></span></p>
+<p class="normal contactType">Phone: <span>414-791-3056</span></p>
+<p class="normal contactType">Regular-Mail:</p>
+<p class="normal contact">
+RKR Composites<br />
+W357 N5895 Meadow Ct<br />
+Oconomowoc,WI 53066
+</p>
+</div>
+</fragment>
---
+++ Products.f
@@ -0,0 +1,85 @@
+<fragment>
+<div class="product">
+   <div class="span-20 clear productHeading">Products:</div>
+   <hr class="sectionSeparator" />
+   <div class="span-20 clear productSubHeading">Masts:</div>
+   <hr class="product" />
+   <div class="span-1 clear spacer">.</div>
+   <div class="span-3 push-1 product" style="margin:0;">
+      <img class="tn" src="products/blackie_tn.jpg" width="100" height="100" />
+   </div>
+   <div class="span-14 product">
+      <p>The <span class="productName">Blackie Carbon</span> mast is made of a combination of carbon-fiber,
+      S-glass, and E-glass laminated with aerospace epoxy and post-cured in an oven.
+      The carbon cloth is on outside of mast with a gloss epoxy finish.<br />
+      To see a mast being built, visit our <span class="std"><a href="Videos.html" >Videos</a></span> page.</p>
+   </div>
+   <div class="span-2 right product">$1400</div>
+   <hr class="product" />
+   <div class="span-1 clear spacer">.</div>
+   <div class="span-3 push-1 product" style="margin:0;">
+      <img class="tn" src="products/blackie_tn.jpg" width="100" height="100" />
+   </div>
+   <div class="span-14 product">
+      <p>The <span class="productName">Black Max</span> has the same layup and construction as the <i>Blackie</i> 
+      but with the carbon-fiber on the inside of the mast. It has a black graphite finish.</p>
+   </div>
+   <div class="span-2 right product">$1400</div>
+   <hr class="product" />
+   <div class="span-1 clear spacer">.</div>
+   <div class="span-3 push-1 product" style="margin:0;">
+      <img class="tn" src="products/twizzler_tn.jpg" width="100" height="100" />
+   </div>
+   <div class="span-14 product">
+      <p>The <span class="productName">Twizzler</span> uses the same construction methods as the <i>Blackie</i> and <i>Black Max</i>
+      but it is bendier for lighter sailors. It can have a dyed finish in red, blue, green, yellow, or grey</p>
+   </div>
+   <div class="span-2 right product">$1400</div>
+   <div class="span-20 clear"><hr class="sectionSeparator" /></div>
+   <div class="span-20 left clear productSubHeading"><span class="productSubHeading">Other Items:</span></div>
+   <hr class="product" />
+   <div class="span-1 clear spacer">.</div>
+   <div class="span-3 push-1 product" style="margin:0;">
+      <img class="tn" src="products/runnerSharpener_tn.jpg" width="100" height="100" />
+   </div>
+   <div class="span-14 product">
+      <p><span class="productName">Benchtop Runner Sharpener</span> For use with all types of runners and all sharpening angles. 
+      Constructed with carbon-fiber re-enforcement for rigidity and stability with a 1hp reversible motor.
+      The sharpener uses readily available 2x90" belts.</p>
+   </div>
+   <div class="span-2 right product">$800</div>
+   <hr class="product" />
+   <div class="span-1 clear spacer">.</div>
+   <div class="span-3 push-1 product" style="margin:0;">
+      <img class="tn" src="products/placeholder.png" width="100" height="100" />
+   </div>
+   <div class="span-14 product">
+      <p><span class="productName">Carbon fiber Runner Stiffeners</span></p>
+   </div>
+   <div class="span-2 right product">$100/pair</div>
+   <hr class="product" />
+   <div class="span-1 clear spacer">.</div>
+   <div class="span-3 push-1 product" style="margin:0;">
+      <img class="tn" src="products/HikingStep3_tn.jpg" width="100" height="100" />
+   </div>
+   <div class="span-14 product">
+      <p><span class="productName">Carbon fiber hiking step</span>
+      Standard 12" or 13&#189;" inch width for Goodwin design hulls or custom cut. 
+      Eaisly mounts with double sided tape or small screws</p>
+   </div>
+   <div class="span-2 right product">$35 each includes shipping</div>
+   <hr class="product" />
+   <div class="span-1 clear spacer">.</div>
+   <div class="span-3 push-1 product" style="margin:0;">
+      <img class="tn" src="products/ExternalHound_tn.jpg" width="100" height="100" />
+   </div>
+   <div class="span-14 product">
+      <p><span class="productName">External Mast Hound</span>
+      Water-Jet Cut 20 gauge Stainless Steel Mast Hound fits Kent Profile can be bent to fit Sherry Masts.<br />
+      Fasten with #8 3/4 SS screws. Good for emergency fix.
+      </p>
+   </div>
+   <div class="span-2 right product">$75 each includes shipping</div>
+
+</div>
+</fragment>
